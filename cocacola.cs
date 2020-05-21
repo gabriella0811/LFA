39e0833298e8fe8cd010051b3bdfb6e5d01bb5a0
gabriella0811 <56557766+gabriella0811@users.noreply.github.com>
2020-05-20 23:21:04 -0300

    Add files via upload

---
+++ cocacola.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valorDaCoca = 0.45;
+            double total = 0;
+            
+            while (total <= valorDaCoca)
+            {
+
+                Console.WriteLine("insira moeda:");
+                double entrada = double.Parse(Console.ReadLine());
+
+                if (entrada == 0.25 || entrada == 0.10)
+                    total += entrada;
+                
+                Console.WriteLine($"total: {total}");
+            }
+
+            Console.WriteLine("coca-cola");
+            Console.ReadLine();
+        }
+    }
+}
